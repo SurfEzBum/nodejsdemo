4ef36a28a0eef93b63c04ffd15fe90c6b6c436f5
Ben Magazino <39971443+SurfEzBum@users.noreply.github.com>
2019-01-21 21:51:40 -0600

    Adding PostgreSQL references and initial code. Breaking code right now.

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 var http = require('http');
+var pg = require('pg');
+var app = express();
+
+var connectionString = "host=bmagnodejs-postgresqldbserver.postgres.database.azure.com port=5432 dbname=postgresqldatabase7545 user=postgresqldbuser@bmagnodejs-postgresqldbserver password={your_password} sslmode=required"
+
+app.get('/', function (req, res, next) {
+    pg.connect(connectionString, function(err, client, done) {
+        if(err) {
+            console.log("not able to get a connection "+ err);
+            res.status(400).send(err);
+        }
+        client.query('Select * from ...', [1], function(error, result) {  // TODO - create database table
+            done(); // closing the connection;
+            if(err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+        });         
+    });
+}); 
 
 var server = http.createServer(function(request, response) {
 
